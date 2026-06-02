dfb1a4fc97681f69aa3f20fb4138f2626348681a
Davin Til <davint2004@gmail.com>
2026-06-02 12:46:09 -0700

    pls lemme lock in for this

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Profile } from './components/Profile'
 
-import './App.css'
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          
+          <Route path='/' element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,30 @@
-import { ProfileNav } from './ProfileNav';
+import { ProfileNav } from './ProfileNav'
+import styles from '../styles/Profile.module.css'
 
 export function Profile() {
+  const exampleProfile = {
+    name: "Davin Til",
+    username: "djurigd",
+    avatar_url: "https://avatars.githubusercontent.com/u/182570390?v=4",
+    followers: 3,
+    following: 3,
+    pronouns: "he/him",
+    bio: "Just testing out."
+  };
+
   return(
     <>
-      <div>
-        
+      <div className={styles.profile_container}>
+        <div id={styles.profile_picture_container}>
+          <img src={exampleProfile.avatar_url} id={styles.profile_picture} />
+        </div>
+        <div className={styles.name_container}>
+          <span id={styles.display_name}>{exampleProfile.name}</span>
+          <span>{exampleProfile.username} - <span>{exampleProfile.pronouns}</span></span>
+          <span>{exampleProfile.followers} followers / {exampleProfile.following} following</span>
+        </div>
       </div>
+      <ProfileNav />
     </>
   )
 }
