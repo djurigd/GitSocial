414d29e410b45d6feab0210ad6b88963a926de0e
Davin Til <davint2004@gmail.com>
2026-06-05 16:51:00 -0700

    post done maybe

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react';
-
 import styles from '../styles/Posts.module.css'
+import { usePost } from './PostContext'
 
-export function Posts({ username }) {
-  const posts = [
-    {
-      title: "Created JSX",
-      description: "Hi, I created a JSX file!",
-      created_at: "2026-05-29",
-      updated_at: "2026-05-29"
-    },
-    {
-      title: "Use JSX",
-      description: "Hi, you should use a JSX file!",
-      created_at: "2026-05-29",
-      updated_at: "2026-05-29"
-    },
-    {
-      title: "Hi!",
-      description: "Hi!",
-      created_at: "2026-05-29",
-      updated_at: "2026-05-29"
-    }
-  ];
+export function Posts() {
+  const { posts } = usePost();
 
   // Update with Postpage integration
   return(
     <>
       <div id={styles.post_container}>
-        { posts.map((post, index) => (
-          <div key={index} id={styles.post}>
+        { posts.map((post) => (
+          <div key={post.id} id={styles.post}>
             <h3>{post.title}</h3>
             <p>{post.description}</p>
             <span>Created at: {post.created_at} / Updated at: {post.updated_at}</span>
